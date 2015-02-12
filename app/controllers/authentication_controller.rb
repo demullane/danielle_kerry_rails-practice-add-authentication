636405d1f3cc6bcda417128c31d06ade2877ca8a
Danielle <danielle@daniellemullane.com>
2015-02-12 15:19:15 -0700

    Add sign-in, sign-up, sign-out and user authentication.

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,19 @@
+class AuthenticationController < ApplicationController
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
+
+end
